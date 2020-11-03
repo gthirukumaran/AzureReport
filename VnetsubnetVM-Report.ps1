e98dbb80f90073ab95c9de0c95b5ea8354eb55c0
gthirukumaran <32206611+gthirukumaran@users.noreply.github.com>
2020-11-03 16:38:29 +0000

    Create VnetsubnetVM-Report.ps1

---
+++ VnetsubnetVM-Report.ps1
@@ -0,0 +1,33 @@
+#Provide the subscription Id where the VMs reside
+$subscriptionId = "xxxx-xxxxxx-xxxxxxx-xxxxxxxxxx"
+
+#Provide the name of the csv file to be exported
+$reportName = "prod.csv"
+
+Select-AzurermSubscription $subscriptionId
+$report = @()
+$vms = Get-AzurermVM
+$publicIps = Get-AzurermPublicIpAddress 
+$nics = Get-AzurermNetworkInterface | ?{ $_.VirtualMachine -NE $null} 
+foreach ($nic in $nics) { 
+    $info = "" | Select VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup 
+    $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id 
+    foreach($publicIp in $publicIps) { 
+        if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
+            $info.PublicIPAddress = $publicIp.ipaddress
+            } 
+        } 
+        $info.OsType = $vm.StorageProfile.OsDisk.OsType 
+        $info.VMName = $vm.Name 
+        $info.ResourceGroupName = $vm.ResourceGroupName 
+        $info.Region = $vm.Location 
+        $info.VmSize = $vm.HardwareProfile.VmSize
+        $info.VirtualNetwork = $nic.IpConfigurations.subnet.Id.Split("/")[-3] 
+        $info.Subnet = $nic.IpConfigurations.subnet.Id.Split("/")[-1] 
+        $info.PrivateIpAddress = $nic.IpConfigurations.PrivateIpAddress 
+        $info.NicName = $nic.Name 
+        $info.ApplicationSecurityGroup = $nic.IpConfigurations.ApplicationSecurityGroups.Id 
+        $report+=$info 
+    } 
+$report | ft VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup 
+$report | Export-CSV "c:\temp\prod.csv"
